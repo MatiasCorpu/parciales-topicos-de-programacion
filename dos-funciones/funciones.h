813e577d266a739f8253610fda1976478f229e01
Matias Corpu <164563483+MatiasCorpu@users.noreply.github.com>
2024-11-01 14:52:15 -0300

    modified funciones.h from dos-funciones

--- dos-funciones/funciones.h
+++ dos-funciones/funciones.h
@@ -11,6 +11,8 @@
 #define TAM_MAX_LINEA 500
 #define TAM_MAX_PALABRA 100
 
+#define TAM_MAX_EQUIPOS 3
+
 #define ES_LETRA(c) ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 
 typedef struct
@@ -27,8 +29,7 @@ typedef struct
 
 typedef int (*Comparar)(const void *, const void *);
 
-
-//FUNCIONES PARTE 1
+// FUNCIONES PARTE 1
 int mstrlen(const char *s);
 char *mstrrchr(const char *s, char c);
 char *mstrcpy(char *sDest, const char *sOrig);
@@ -38,60 +39,12 @@ int compararPalabras(const Palabra *pal1, const Palabra *pal2);
 int palabraMasLargaAparicionesYcantPalabrasArchTxt(Palabra *palLarga, char *linea, int *cantPalEnArch);
 Palabra *contarPalabrasMio(const char *nomArchTxt, Palabra *pal);
 
-/*
-Parte 2-esMatrizCampeonatoValidaMio 
-Se dispone de una matriz cuadrada de enteros de orden N, donde cada elemento [i][j] representa la 
-cantidad de puntos que obtuvo el equipo i frente al equipo j al fin de un torneo de fútbol (partidos 
-de ida y vuelta) en el que participaron N equipos. El sistema de puntuación es: 3 puntos para el 
-ganador del partido y ninguno para el perdedor; 1 punto para cada equipo en caso de empate. 
-Implemente la función esMatrizCampeonatoValidaMIO que determine si la matriz está 
-correctamente generada.
-*/
-
-// FUCIONES PARTE 2
-
-
-
-//posibles resultados:
-/*
-          river  boca   sanLor
-river  =         3+1
-boca   =  0+1
-sanLor =  
-
-3 = 0
-1 = 1
-0 = 3
-
- 1 + 1
- 1 + 1
-
-6 - 2 = 4
-
-3+3 = 6 ->j = 0 + 0 = 0
-3+1 = 4 ->j = 0 + 1 = 1
-3+0 = 3 ->j = 0 + 3 = 3
-1+1 = 2 ->j = 1 + 1 = 2
-0+0 = 0 ->j = 3 + 3 = 6
-
-        0 1 2
-river 0{0,6,2},
-boca  1{0,0,3},
-san l 2{2,3,0},
-
-
-
-*/
-#define TAM_MAX_EQUIPOS 3
-
+// FUNCIONES PARTE 2
 void **matrizCrear(int filas, int columnas, size_t tamElem);
 void matrizDestruir(void **mat, int filas);
-
 void matrizLLenarDatosCampeonato(int **mat, int filas, int columnas, int matCampeonato[][TAM_MAX_EQUIPOS]);
 void matrizMostrar(int **mat, int filas, int columnas);
 bool esMatrizCampeonatoValidaMIO(const int **mat, int filCol);
-
-
 int cmpEnteros(const void *a, const void *b);
 void *busquedaBinaria(const void *v, int ce, size_t tamElem, const void *elem, Comparar cmp);
 
